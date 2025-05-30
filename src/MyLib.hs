ecf5d6c7f2d19627af97556ea88a2050996689e3
Riyo <ymdfield@outlook.jp>
2025-05-30 12:09:45 +0900

    initial

---
+++ src/MyLib.hs
@@ -0,0 +1,6 @@
+-- SPDX-License-Identifier: MPL-2.0
+
+module MyLib (someFunc) where
+
+someFunc :: IO ()
+someFunc = putStrLn "someFunc"
---
+++ test/Main.hs
@@ -0,0 +1,6 @@
+-- SPDX-License-Identifier: MPL-2.0
+
+module Main (main) where
+
+main :: IO ()
+main = putStrLn "Test suite not yet implemented."
