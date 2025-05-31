cf3ccfb91ba161272dae9a4320b7f71b546cbcbb
Riyo <ymdfield@outlook.jp>
2025-05-31 13:39:18 +0900

    add comments

--- src/Control/Monad/Effect.hs
+++ src/Control/Monad/Effect.hs
@@ -18,21 +18,29 @@ import UnliftIO (MonadIO, MonadUnliftIO, liftIO, withRunInIO)
 type Effect' = (Type -> Type) -> Type -> Type
 data Effect = E ((Type -> Type) -> Type -> Type) Resumption
 
+{- | Types of resumption.
+
+@NonTail ans r@: where @ans@ is the answer type and @r@ is the underlying prompt frame list.
+-}
 data Resumption = Tail | NonTail Type [Type]
 
+-- | A effect handler.
 data Handler e f b where
     TailResumptive :: (forall x. e f x -> CtlT fs b x) -> Handler ('E e Tail) f b
     NonTailResumptive :: (forall fs x. (Member ans r fs) => e f x -> CtlT fs b x) -> Handler ('E e (NonTail ans r)) f b
 
+-- A contravariant map on handlers for higher-order effects.
 hcmapHandler :: (HFunctor e) => (forall x. f x -> g x) -> Handler ('E e r) g b -> Handler ('E e r) f b
 hcmapHandler phi = \case
     TailResumptive h -> TailResumptive $ h . hfmap phi
     NonTailResumptive h -> NonTailResumptive $ h . hfmap phi
 
+-- | Vector of handlers.
 data HandlerVec (es :: [Effect]) f b where
     Cons :: Handler e f b -> Handlers es f b -> HandlerVec (e : es) f b
     Nil :: HandlerVec '[] f b
 
+-- | Type-level search over elements in a vector.
 class (HFunctor ff) => Elem ff (es :: [Effect]) res | ff es -> res where
     getHandler :: Handlers es f b -> Handler ('E ff res) f b
     updateHandler :: Handler ('E ff res) f b -> Handlers es f b -> Handlers es f b
@@ -45,6 +53,7 @@ instance {-# OVERLAPPABLE #-} (Elem ff es res, HFunctor ff') => Elem ff ('E ff' 
     getHandler (Handlers (Cons _ hs) koi) = hcmapHandler koi $ getHandler hs
     updateHandler h (Handlers (Cons h' hs) koi) = Handlers (Cons (hcmapHandler koi h') (updateHandler h $ hcmapHandlers koi hs)) id
 
+-- | Equip the handler vector with a free HFunctor structure.
 data Handlers es f b = forall g. Handlers
     { handlers :: HandlerVec es g b
     , koi :: forall x. f x -> g x
@@ -53,12 +62,15 @@ data Handlers es f b = forall g. Handlers
 hcmapHandlers :: (forall x. f x -> g x) -> Handlers es g b -> Handlers es f b
 hcmapHandlers phi (Handlers f koi) = Handlers f (koi . phi)
 
+-- | A type-class for higher-order effects.
 class HFunctor ff where
     hfmap :: (forall x. f x -> g x) -> ff f a -> ff g a
 
+-- | Prepend to the handler vector.
 (!:) :: Handler e f b -> Handlers es f b -> Handlers (e : es) f b
 h !: hs = Handlers (Cons h hs) id
 
+-- | An effect monad transformer built on top of a multi-prompt/control monad.
 newtype EffT es fs b a
     = EffT {unEffT :: Handlers es (EffT es fs b) b -> CtlT fs b a}
     deriving (Functor)
@@ -88,6 +100,7 @@ withEffToCtl f = EffT \v -> f (runEffT v)
 trans :: (Handlers es' (EffT es' fs b) b -> Handlers es (EffT es' fs b) b) -> EffT es fs b a -> EffT es' fs b a
 trans f (EffT withHandlerVec) = EffT $ withHandlerVec . hcmapHandlers (trans f) . f
 
+-- | Interpret a tail-resumptive effect.
 interpret ::
     (forall x. e (EffT es fs b) x -> EffT es fs b x) ->
     EffT ('E e 'Tail : es) fs b a ->
@@ -119,6 +132,7 @@ interpretNT ::
 interpretNT f (EffT m) = EffT \v -> prompt \p -> undefined
 -}
 
+-- | A send that works with both kinds of resumption.
 class (Elem ff es res) => Send res ff es fs where
     send :: ff (EffT es fs b) a -> EffT es fs b a
 
