45fc65a1317a9dd12fcdb06014eb017b58577070
Riyo <ymdfield@outlook.jp>
2025-07-28 09:47:33 +0900

    minimal prompt-frame-erased Eff monad

--- src/Control/Monad/Effect.hs
+++ src/Control/Monad/Effect.hs
@@ -39,13 +39,16 @@ data Sub (u :: [k]) (xs :: [k])
     }
 
 instance {-# INCOHERENT #-} (u < xs) => u < x : xs where
-    sub p =
-        Sub
-            { weaken = There . weaken (sub p)
-            , strengthen = \case
-                Here _ -> Nothing
-                There u -> strengthen (sub p) u
-            }
+    sub p = weakenSub (sub p)
+
+weakenSub :: Sub u xs -> Sub u (x : xs)
+weakenSub s =
+    Sub
+        { weaken = There . weaken s
+        , strengthen = \case
+            Here _ -> Nothing
+            There u -> strengthen s u
+        }
 
 instance xs < xs where
     sub _ = Sub{weaken = id, strengthen = Just}
@@ -53,23 +56,23 @@ instance xs < xs where
 type Effect = Type -> Type
 type Prompt = Type -> Type
 
-data Handler p e where
-    Handler :: (forall w u es x. e x -> Sub (p : u) w -> Ctl w es x) -> Handler p e
+data Handler ps e where
+    Handler :: (forall w u' es x. e x -> Sub (p : u') w -> Ctl w es x) -> Sub (p : u) ps -> Handler ps e
 
 data Handlers ps es where
-    Cons :: Handler p e -> Handlers ps es -> Handlers (p : ps) (e : es)
-    Nil :: Handlers ps '[]
+    Cons :: Handler (p : ps) e -> Handlers ps es -> Handlers (p : ps) (e : es)
+    Nil :: Handlers '[] '[]
 
-newtype Eff ps es a = Eff {unEff :: Ctl ps es a}
+newtype Eff es a = Eff {unEff :: forall ps. Ctl ps es a}
 
-instance Functor (Eff ps es) where
+instance Functor (Eff es) where
     fmap f = (>>= pure . f)
 
-instance Applicative (Eff ps es) where
+instance Applicative (Eff es) where
     pure x = Eff \_ -> Pure x
     (<*>) = ap
 
-instance Monad (Eff ps es) where
+instance Monad (Eff es) where
     Eff m >>= f = Eff \hs -> case m hs of
         Pure x -> unEff (f x) hs
         Freer u k -> Freer u (k >=> f)
@@ -78,27 +81,28 @@ type Ctl (ps :: [Prompt]) (es :: [Effect]) a = Handlers ps es -> EffCtlF ps es a
 
 data EffCtlF ps es a
     = Pure a
-    | forall x. Freer (StackUnion ps Control x) (x -> Eff ps es a)
+    | forall x. Freer (StackUnion ps Control x) (x -> Eff es a)
 
 data Control (f :: Prompt) (u :: [Prompt]) a where
-    Control :: (forall w es x. (a -> Eff w es (f x)) -> Eff w es (f x)) -> Control f u a
+    Control :: (forall es x. (a -> Eff es (f x)) -> Eff es (f x)) -> Control f u a
 
-data Membership e es p ps = forall u. Membership {getHandler :: Handlers ps es -> Handler p e, getPrompt :: Sub (p : u) ps}
+newtype Membership e es = Membership {getHandler :: forall ps. Handlers ps es -> Handler ps e}
 
-hereMembership :: Membership e (e : es) p (p : ps)
-hereMembership = Membership (\(Cons h _) -> h) (Sub id Just)
+hereMembership :: Membership e (e : es)
+hereMembership = Membership (\(Cons h _) -> h)
 
-send :: Membership e es p ps -> e a -> Eff ps es a
-send (Membership getHandler p) e = Eff \hs -> let Handler h = getHandler hs in h e p hs
+send :: Membership e es -> e a -> Eff es a
+send (Membership getHandler) e = Eff \hs -> case getHandler hs of
+    Handler h i -> h e i hs
 
-control :: Sub (p : u) ps -> (forall w es' x. (a -> Eff w es' (p x)) -> Eff w es' (p x)) -> Ctl ps es a
+control :: Sub (p : u) ps -> (forall es' x. (a -> Eff es' (p x)) -> Eff es' (p x)) -> Ctl ps es a
 control i f _ = Freer (weaken i $ Here $ Control f) pure
 
-interpret :: (forall w es' x y. e x -> (x -> Eff w es' (p y)) -> Eff w es' (p y)) -> Eff (p : ps) (e : es) (p a) -> Eff ps es (p a)
+interpret :: (forall es' x (y :: Type). e x -> (x -> Eff es' (p y)) -> Eff es' (p y)) -> Eff (e : es) (p a) -> Eff es (p a)
 interpret hdl (Eff m) =
-    let h = Handler \e i -> control i \k -> hdl e k
-     in Eff \hs ->
-            case m (Cons h hs) of
+    Eff \hs ->
+        let hs' = Cons (Handler (\e i -> control i \k -> hdl e k) $ Sub id Just) hs
+         in case m hs' of
                 Pure x -> Pure x
                 Freer ctls k -> case ctls of
                     Here (Control ctl) -> unEff (ctl $ interpret hdl . k) hs
@@ -107,13 +111,13 @@ interpret hdl (Eff m) =
 data NonDet :: Effect where
     Choose :: NonDet Bool
 
-runNonDet :: Eff ([] : ps) (NonDet : es) [a] -> Eff ps es [a]
+runNonDet :: Eff (NonDet : es) [a] -> Eff es [a]
 runNonDet = interpret \Choose k -> do
     xs <- k False
     ys <- k True
     pure $ xs ++ ys
 
-runPure :: Eff '[] '[] a -> a
+runPure :: Eff '[] a -> a
 runPure (Eff m) = case m Nil of
     Pure x -> x
     Freer u _ -> case u of {}
