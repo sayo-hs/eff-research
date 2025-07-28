ea69826467133321fee8cb2c6383965d518906f0
Riyo <ymdfield@outlook.jp>
2025-07-28 18:01:27 +0900

    refactor

--- src/Control/Monad/Effect.hs
+++ src/Control/Monad/Effect.hs
@@ -1,22 +1,12 @@
-{-# LANGUAGE AllowAmbiguousTypes #-}
-{-# LANGUAGE DerivingVia #-}
-{-# LANGUAGE FunctionalDependencies #-}
-{-# LANGUAGE QuantifiedConstraints #-}
 {-# LANGUAGE TypeData #-}
-{-# LANGUAGE TypeFamilyDependencies #-}
-{-# LANGUAGE UndecidableInstances #-}
-{-# HLINT ignore "Avoid lambda" #-}
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
 -- SPDX-License-Identifier: MPL-2.0
 
 module Control.Monad.Effect where
 
 import Control.Monad (ap, join, (>=>))
-import Data.Function ((&))
 import Data.Functor.Identity (Identity (Identity, runIdentity))
 import Data.Kind (Type)
-import Data.Type.Equality ((:~:) (Refl))
 
 data Union (xs :: [k]) (h :: k -> l -> Type) (a :: l) where
     Here :: h x a -> Union (x : xs) h a
@@ -66,7 +56,10 @@ type Effect = Type -> Type
 type data Prompt = P (Type -> Type) [Effect]
 
 data Handler ps e where
-    Handler :: (forall w es x. e x -> Membership p w -> Ctl w es x) -> Membership p ps -> Handler ps e
+    Handler ::
+        (forall w es' x. Membership (P f es) w -> e x -> Ctl w es' x) ->
+        Membership (P f es) ps ->
+        Handler ps e
 
 data Handlers ps es where
     ConsHandler :: Handler ps e -> Handlers ps es -> Handlers ps (e : es)
@@ -79,18 +72,18 @@ instance Functor (Eff es) where
     fmap f = (>>= pure . f)
 
 instance Applicative (Eff es) where
-    pure x = Eff \_ -> Pure x
+    pure x = Eff $ Ctl \_ -> Pure x
     (<*>) = ap
 
 instance Monad (Eff es) where
-    Eff m >>= f = Eff \hs -> case m hs of
-        Pure x -> unEff (f x) hs
+    Eff m >>= f = Eff $ Ctl \hs -> case unCtl m hs of
+        Pure x -> unCtl (unEff (f x)) hs
         Freer u k -> Freer u (k >=> f)
 
 trans :: (forall ps. Handlers ps es' -> Handlers ps es) -> Eff es a -> Eff es' a
 trans f (Eff m) =
-    Eff \hs ->
-        case m (f hs) of
+    Eff $ Ctl \hs ->
+        case unCtl m (f hs) of
             Pure x -> Pure x
             Freer u k -> Freer u $ trans f . k
 
@@ -100,7 +93,7 @@ raise = trans \(ConsHandler _ hs) -> hs
 swap :: Handlers ps (e1 : e2 : es) -> Handlers ps (e2 : e1 : es)
 swap (ConsHandler h1 (ConsHandler h2 es)) = ConsHandler h2 (ConsHandler h1 es)
 
-type Ctl (ps :: [Prompt]) (es :: [Effect]) a = Handlers ps es -> EffCtlF ps es a
+newtype Ctl (ps :: [Prompt]) (es :: [Effect]) a = Ctl {unCtl :: Handlers ps es -> EffCtlF ps es a}
 
 data EffCtlF ps es a
     = Pure a
@@ -125,7 +118,7 @@ weakenHandlerMembership i = HandlerMembership \case
 weakenPrompt :: Handler ps e -> Handler (p : ps) e
 weakenPrompt (Handler h i) = Handler h (weakenMembership i)
 
-liftPrompt :: Handlers ps es -> Handlers (p : ps) es
+liftPrompt :: forall p ps es. Handlers ps es -> Handlers (p : ps) es
 liftPrompt = \case
     ConsHandler h hs -> ConsHandler (weakenPrompt h) (liftPrompt hs)
     NilHandler -> ConsPrompt NilHandler
@@ -141,27 +134,64 @@ instance {-# OVERLAPPABLE #-} (e :> es) => e :> (e' : es) where
     handlerMembership = weakenHandlerMembership handlerMembership
 
 send :: HandlerMembership e es -> e a -> Eff es a
-send i e = Eff \hs -> case atHandler i hs of
-    Handler h i' -> h e i' hs
+send i e = Eff $ Ctl \hs -> case atHandler i hs of
+    Handler h i' -> unCtl (h i' e) hs
 
 perform :: (e :> es) => e a -> Eff es a
 perform = send handlerMembership
 
 control :: Membership (P f u) ps -> (forall x. (a -> Eff u (f x)) -> Eff u (f x)) -> Ctl ps es a
-control i f _ = Freer (inject i $ Control f) pure
+control i f = Ctl \_ -> Freer (inject i $ Control f) pure
 
-interpret :: (forall x (y :: Type). e x -> (x -> Eff es (f y)) -> Eff es (f y)) -> Eff (e : es) (f a) -> Eff es (f a)
-interpret hdl (Eff m) =
-    Eff \hs ->
-        let hs' = ConsHandler (Handler (\e i -> control i \k -> hdl e k) membership0) (liftPrompt hs)
-         in case m hs' of
+pureCtl :: a -> Ctl ps es a
+pureCtl x = Ctl \_ -> Pure x
+
+bindCtl :: Ctl ps es a -> (a -> Eff es b) -> Ctl ps es b
+bindCtl (Ctl m) f = Ctl \hs -> case m hs of
+    Pure x -> unCtl (unEff $ f x) hs
+    Freer u k -> Freer u (k >=> f)
+
+fmapCtl :: (a -> b) -> Ctl ps es a -> Ctl ps es b
+fmapCtl f m = m `bindCtl` (pure . f)
+
+{-
+delimit :: Membership (P f u) ps -> Ctl ps es (f a) -> Ctl ps es (f a)
+delimit i (Ctl m) = Ctl \hs ->
+    case m hs of
+        Pure x -> Pure x
+        Freer ctls k -> case project i ctls of
+            Just (Control ctl) -> unCtl (unEff $ undefined $ ctl $ trans undefined . k) hs
+            Nothing -> Freer ctls k
+-}
+
+interpretShallow ::
+    (forall w esSend x. Membership (P f (e : es)) w -> e x -> Ctl w esSend x) ->
+    Eff (e : es) (f a) ->
+    Eff es (f a)
+interpretShallow h (Eff m) =
+    Eff $ Ctl \hs ->
+        let hs' = ConsHandler (Handler h membership0) (liftPrompt hs)
+         in case unCtl m hs' of
                 Pure x -> Pure x
                 Freer ctls k -> case ctls of
-                    Here (Control ctl) -> unEff (ctl $ interpret hdl . k) hs
-                    There u -> Freer u $ interpret hdl . k
+                    Here (Control ctl) -> unCtl (unEff $ interpretShallow h $ ctl k) hs
+                    There u -> Freer u $ interpretShallow h . k
+
+interpret ::
+    (forall w esSend x. Membership (P f es) w -> e x -> Ctl w esSend x) ->
+    Eff (e : es) (f a) ->
+    Eff es (f a)
+interpret h (Eff m) =
+    Eff $ Ctl \hs ->
+        let hs' = ConsHandler (Handler h membership0) (liftPrompt hs)
+         in case unCtl m hs' of
+                Pure x -> Pure x
+                Freer ctls k -> case ctls of
+                    Here (Control ctl) -> unCtl (unEff $ ctl $ interpret h . k) hs
+                    There u -> Freer u $ interpret h . k
 
 runPure :: Eff '[] a -> a
-runPure (Eff m) = case m NilHandler of
+runPure (Eff m) = case unCtl m NilHandler of
     Pure x -> x
     Freer u _ -> case u of {}
 
@@ -169,7 +199,7 @@ data NonDet :: Effect where
     Choose :: NonDet Bool
 
 runNonDet :: Eff (NonDet : es) [a] -> Eff es [a]
-runNonDet = interpret \Choose k -> do
+runNonDet = interpret \i Choose -> control i \k -> do
     xs <- k False
     ys <- k True
     pure $ xs ++ ys
@@ -179,21 +209,21 @@ runNonDet = interpret \Choose k -> do
 
 test :: [(Bool, Bool)]
 test = runPure $ runNonDet do
-    b1 <- send handlerMembership0 Choose
-    b2 <- send handlerMembership0 Choose
+    b1 <- perform Choose
+    b2 <- perform Choose
     pure [(b1, b2)]
 
 data Reader r :: Effect where
     Ask :: Reader r r
 
 runReader :: r -> Eff (Reader r : es) a -> Eff es a
-runReader r = fmap runIdentity . interpret (\Ask k -> k r) . fmap Identity
+runReader r = fmap runIdentity . interpret (\_ Ask -> Ctl \_ -> Pure r) . fmap Identity
 
 data Evil :: Effect where
     Evil :: Evil ()
 
 runEvil :: Eff (Evil : es) a -> Eff es (Eff es a)
-runEvil = interpret (\Evil k -> pure $ join $ k ()) . fmap pure
+runEvil = interpret (\i Evil -> control i \k -> pure $ join $ k ()) . fmap pure
 
 -- >>> testNSR
 -- (1,2)
