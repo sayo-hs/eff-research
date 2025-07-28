2cfcdf13cdefb5ecba7c0cdeedd6eefe7d70d2b3
Riyo <ymdfield@outlook.jp>
2025-07-28 15:45:15 +0900

    decoupling prompt frame stack and effect list

--- src/Control/Monad/Effect.hs
+++ src/Control/Monad/Effect.hs
@@ -13,27 +13,22 @@
 module Control.Monad.Effect where
 
 import Control.Monad (ap, join, (>=>))
-import Data.Functor.Const (Const)
 import Data.Functor.Identity (Identity (Identity, runIdentity))
 import Data.Kind (Type)
-import Data.Proxy (Proxy (Proxy))
 
 data Union (xs :: [k]) (h :: k -> l -> Type) (a :: l) where
     Here :: h x a -> Union (x : xs) h a
     There :: Union xs h a -> Union (x : xs) h a
 
-{-
 data Rec (xs :: [k]) (h :: k -> l -> Type) (a :: l) where
     Cons :: h x a -> Rec xs h a -> Rec (x : xs) h a
     Nil :: Rec '[] h a
--}
 
 data Membership x xs = Membership
     { inject :: forall l h (a :: l). h x a -> Union xs h a
     , project :: forall l h (a :: l). Union xs h a -> Maybe (h x a)
-    {-, at :: forall l h (a :: l). Rec xs h a -> h x a
+    , at :: forall l h (a :: l). Rec xs h a -> h x a
     , update :: forall l h (a :: l). h x a -> Rec xs h a -> Rec xs h a
-    -}
     }
 
 membership0 :: Membership x (x : xs)
@@ -43,11 +38,8 @@ membership0 =
         \case
             Here x -> Just x
             _ -> Nothing
-
-{-
-(\(Cons h _) -> h)
-(\h (Cons _ hs) -> Cons h hs)
--}
+        (\(Cons h _) -> h)
+        (\h (Cons _ hs) -> Cons h hs)
 
 weakenMembership :: Membership x xs -> Membership x (x' : xs)
 weakenMembership i =
@@ -56,11 +48,8 @@ weakenMembership i =
         \case
             Here _ -> Nothing
             There u -> project i u
-
-{-
-(\(Cons _ hs) -> at i hs)
-(\h (Cons h' hs) -> Cons h' $ update i h hs)
--}
+        (\(Cons _ hs) -> at i hs)
+        (\h (Cons h' hs) -> Cons h' $ update i h hs)
 
 class Member x xs where
     membership :: Membership x xs
@@ -78,8 +67,9 @@ data Handler ps e where
     Handler :: (forall w es x. e x -> Membership p w -> Ctl w es x) -> Membership p ps -> Handler ps e
 
 data Handlers ps es where
-    Cons :: Handler (p : ps) e -> Handlers ps es -> Handlers (p : ps) (e : es)
-    Nil :: Handlers '[] '[]
+    ConsHandler :: Handler ps e -> Handlers ps es -> Handlers ps (e : es)
+    ConsPrompt :: Handlers ps es -> Handlers (p : ps) es
+    NilHandler :: Handlers '[] '[]
 
 newtype Eff es a = Eff {unEff :: forall ps. Ctl ps es a}
 
@@ -110,10 +100,17 @@ newtype HandlerMembership e es
     }
 
 handlerMembership0 :: HandlerMembership e (e : es)
-handlerMembership0 = HandlerMembership (\(Cons h _) -> h)
+handlerMembership0 = HandlerMembership \case
+    ConsHandler h _ -> h
+    ConsPrompt hs -> weakenPrompt $ atHandler handlerMembership0 hs
 
 weakenHandlerMembership :: HandlerMembership e es -> HandlerMembership e (e' : es)
-weakenHandlerMembership i = HandlerMembership (\(Cons _ hs) -> liftPrompt $ atHandler i hs)
+weakenHandlerMembership i = HandlerMembership \case
+    ConsHandler _ hs -> atHandler i hs
+    ConsPrompt hs -> weakenPrompt $ atHandler (weakenHandlerMembership i) hs
+
+weakenPrompt :: Handler ps e -> Handler (p : ps) e
+weakenPrompt (Handler h i) = Handler h (weakenMembership i)
 
 class e :> es where
     handlerMembership :: HandlerMembership e es
@@ -123,9 +120,6 @@ instance e :> (e : es) where
 
 instance {-# OVERLAPPABLE #-} (e :> es) => e :> (e' : es) where
     handlerMembership = weakenHandlerMembership handlerMembership
-
-liftPrompt :: Handler ps e -> Handler (p : ps) e
-liftPrompt (Handler h i) = Handler h (weakenMembership i)
 
 send :: HandlerMembership e es -> e a -> Eff es a
 send i e = Eff \hs -> case atHandler i hs of
@@ -140,7 +134,7 @@ control i f _ = Freer (inject i $ Control f) pure
 interpret :: (forall x (y :: Type). e x -> (x -> Eff es (f y)) -> Eff es (f y)) -> Eff (e : es) (f a) -> Eff es (f a)
 interpret hdl (Eff m) =
     Eff \hs ->
-        let hs' = Cons (Handler (\e i -> control i \k -> hdl e k) membership0) hs
+        let hs' = ConsHandler (Handler (\e i -> control i \k -> hdl e k) membership0) (ConsPrompt hs)
          in case m hs' of
                 Pure x -> Pure x
                 Freer ctls k -> case ctls of
@@ -148,7 +142,7 @@ interpret hdl (Eff m) =
                     There u -> Freer u $ interpret hdl . k
 
 runPure :: Eff '[] a -> a
-runPure (Eff m) = case m Nil of
+runPure (Eff m) = case m NilHandler of
     Pure x -> x
     Freer u _ -> case u of {}
 
